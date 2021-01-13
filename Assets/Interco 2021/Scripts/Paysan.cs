1cc87ec90972c59d9554e2af7820b61801ceb294
Thomas O Fredericks <tof@t-o-f.info>
2021-01-13 15:54:51 -0500

    Initial commit

---
+++ Assets/Interco 2021/Scripts/Paysan.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paysan : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Vector2 impulse = new Vector2(1, 1);
+    public float torque = -0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.AddTorque(torque, ForceMode2D.Impulse);
+            rb.AddForce(impulse, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Interco 2021/Scripts/Vent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vent : MonoBehaviour
+{
+    public float amount = 10f;
+    public float speed = 1.5f;
+    float phase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        phase = Random.Range(0, Mathf.PI * 2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zRotation = Mathf.Cos(phase) * amount;
+        phase += speed * Time.deltaTime;
+   
+        Quaternion target = Quaternion.Euler(0, 0, zRotation);
+        transform.rotation = target;
+    }
+}
