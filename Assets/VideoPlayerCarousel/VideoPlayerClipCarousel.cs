391366966dfa256fcf4023bb3eb04951a4aa6ec2
Thomas O Fredericks <tof@t-o-f.info>
2021-01-14 11:55:41 -0500

    Added Video Player Carousel

---
+++ Assets/VideoPlayerCarousel/VideoPlayerClipCarousel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayerClipCarousel: MonoBehaviour
+{
+    UnityEngine.Video.VideoPlayer videoPlayer;
+    public UnityEngine.Video.VideoClip[]  videoClips;
+    int videoIndex = 0;
+    void Start()
+    {
+        videoPlayer = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>();
+    }
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            videoIndex = (videoIndex + 1) % videoClips.Length;
+            videoPlayer.clip = videoClips[videoIndex];
+        }
+    }
+}
+
